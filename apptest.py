4084ddb662a9fa237e14ee08ed8a7568cf71373c
melinda-pol antonio dazo <melinda_2212@hotmail.com>
2020-12-03 16:59:05 +0100

    update

--- apptest.py
+++ apptest.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 st.title("Video Game EDA")
 activity = ["Basico EDA", "Multiplataforma","Geografía","Género","ESRB","Acuerdo"]
-choice = st.sidebar.selectbox("Main activity",activity)
+choice = st.sidebar.selectbox("Tipo de análisis",activity)
 st.markdown("[Fuente: Kaggle video game sales](https://www.kaggle.com/ashaheedq/video-games-sales-2019)")
 #Dataset Kaggle
 df0 = pd.read_csv("vgsales-12-4-2019.csv")
@@ -24,6 +24,19 @@ def main():
         st.write(df0.dtypes)
         st.text("¿Datos nulos?:")
         st.write(df0.isnull().sum())
+
+    if choice == "Multiplataforma":
+        fig1 = px.pie(df0, values=df0.Global_Sales,names=df0.Platform,color=df0.Platform)
+        fig1.update_layout(title="<b>Sales pecentage between platforms</b>")
+        st.plotly_chart(fig1)
+    if choice == "Geografía":
+        pass
+    if choice == "Género":
+        pass
+    if choice == "ESRB":
+        pass
+    if choice == "Acuerdo":
+        pass
 
 if __name__ == '__main__':
     main()
