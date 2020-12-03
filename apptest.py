a6c4701aad8736bf699de6f4b94134be8dfb3af5
melinda-pol antonio dazo <melinda_2212@hotmail.com>
2020-12-03 12:04:45 +0100

    basiceda

--- apptest.py
+++ apptest.py
@@ -10,3 +10,20 @@ choice = st.sidebar.selectbox("Main activity",activity)
 st.markdown("[Fuente: Kaggle video game sales](https://www.kaggle.com/ashaheedq/video-games-sales-2019)")
 #Dataset Kaggle
 df0 = pd.read_csv("vgsales-12-4-2019.csv")
+
+def main():
+    if choice == "Basico EDA":
+        st.title('Análisis básico')
+        st.text("¿Tamaño del dataset?")
+        st.write(df0.shape)
+        st.text("5 primeras filas:")
+        st.write(df0.head(5))
+        st.text("Descripción general:")
+        st.write(df0.describe(include='all'))
+        st.text("¿Qué tipo de datos tengo?")
+        st.write(df0.dtypes)
+        st.text("¿Datos nulos?:")
+        st.write(df0.isnull().sum())
+
+if __name__ == '__main__':
+    main()
