5e9afbb9c65e60a9773fd287e141726397609498
melinda-pol antonio dazo <melinda_2212@hotmail.com>
2020-12-03 12:02:20 +0100

    update

--- apptest.py
+++ apptest.py
@@ -4,5 +4,9 @@ import numpy as np
 import altair as alt
 from datetime import datetime
 
-st.title("Streamlit sharing service")
-st.subheader("Amaizing, works!")
+st.title("Video Game EDA")
+activity = ["Basico EDA", "Multiplataforma","Geografía","Género","ESRB","Acuerdo"]
+choice = st.sidebar.selectbox("Main activity",activity)
+st.markdown("[Fuente: Kaggle video game sales](https://www.kaggle.com/ashaheedq/video-games-sales-2019)")
+#Dataset Kaggle
+df0 = pd.read_csv("vgsales-12-4-2019.csv")
