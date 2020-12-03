05ec72d0a77e046f34758937329e9dbb67f49ae3
melinda-pol antonio dazo <melinda_2212@hotmail.com>
2020-12-03 11:59:18 +0100

    update

--- apptest.py
+++ apptest.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+from datetime import datetime
 
 st.title("Streamlit sharing service")
 st.subheader("Amaizing, works!")
